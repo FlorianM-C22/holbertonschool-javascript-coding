56a37da7050a1b3aceec4b717c5b7f7f7890a98c
FlorianM-C22 <7484@holbertonstudents.com>
2024-05-13 13:48:19 +0200

    Task 2 - Semistandard correction

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -2,7 +2,7 @@
 
 const request = require('request');
 
-request(process.argv[2], function(error, response) {
+request(process.argv[2], function (error, response) {
   if (error) console.log(error);
   else console.log('code:', response.statusCode);
 });
