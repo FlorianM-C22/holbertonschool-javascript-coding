f9e0a9a1f8f38e34b7b1eb49e45797b9b45c17da
FlorianM-C22 <7484@holbertonstudents.com>
2024-05-22 14:43:49 +0200

    Task 1 - Again again

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,10 +1,15 @@
-process.stdout.write('Welcome to Holberton School, what is your name?\n');
+/* eslint-disable no-undef */
+function welcome() {
+  process.stdout.write('Welcome to Holberton School, what is your name?\n');
+  process.stdin.on('data', (input) => {
+    process.stdout.write(`Your name is: ${input.toString()}`);
+    process.stdout.write('This important software is now closing\n');
+    process.exit();
+  });
+}
 
-process.stdin.on('data', (data) => {
-  const name = data.toString().trim();
-  process.stdout.write(`Your name is: ${name}\n`);
-});
+module.exports = welcome;
 
-process.on('exit', () => {
-  process.stdout.write('This important software is now closing\n');
-});
+if (require.main === module) {
+  welcome();
+}
